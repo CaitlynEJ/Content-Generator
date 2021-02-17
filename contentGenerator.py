365ff572d1e5f6320e0064c8babcb50840333e69
Caitlyn Jameson <59493115+CaitlynEJ@users.noreply.github.com>
2021-02-17 14:27:00 -0600

    update file

--- contentGenerator.py
+++ contentGenerator.py
@@ -1,13 +1,29 @@
 from tkinter import *
+import requests
+from bs4 import BeautifulSoup
 
 root = Tk()
 root.title("Content Generator")
 
+# define generateParagrah function
+
+
+def generateParagrah():
+    primaryKey = inputBox1.get()
+    secondKey = inputBox2.get()
+
+    response = requests.get(
+        url="https://en.wikipedia.org/wiki/" + primaryKey)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = soup.findall("p")
+
+
 # define header
 header = Label(root, text="Content Generator")
 
-inputFrame = LabelFrame(root, padx=10, pady= 10)
-outputFrame = LabelFrame(root, padx=10, pady= 10)
+inputFrame = LabelFrame(root, padx=10, pady=10)
+outputFrame = LabelFrame(root, padx=10, pady=10)
 
 # define Input text box for primary keyword
 primaryKeyword = Label(inputFrame, text="Please enter a primary keyword.")
@@ -18,15 +34,16 @@ secondaryKeyword = Label(inputFrame, text="Please enter a secondary keyword.")
 inputBox2 = Entry(inputFrame, width=20, borderwidth=5)
 
 # define submit button
-submitButton = Button(inputFrame, text="Submit Keywords!")
+submitButton = Button(inputFrame, text="Submit Keywords!",
+                      command=generateParagraph)
 
 
 # define output text box
 outputBox = Entry(outputFrame, borderwidth=5)
-resultHeader = Label(outputFrame, text="Results will be outputted below.")
+resultHeader = Label(outputFrame, text="Generated results:")
 
 # layout grid
-inputFrame.grid(row=0, column=0,padx=10, pady=10)
+inputFrame.grid(row=0, column=0, padx=10, pady=10)
 outputFrame.grid(row=1, column=0, padx=10, pady=10)
 
 header.grid(row=0, column=0)
@@ -39,7 +56,8 @@ inputBox2.grid(row=2, column=1)
 submitButton.grid(row=3, column=1)
 
 resultHeader.grid(row=4, column=0)
-outputBox.grid(row=5, column=0, columnspan=3, padx=10, pady=10, ipadx=100, ipady=50)
+outputBox.grid(row=5, column=0, columnspan=3,
+               padx=10, pady=10, ipadx=100, ipady=50)
 
 
 root.mainloop()
