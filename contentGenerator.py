f19595dd40d29719fa596d222b42864993a5eb7f
Caitlyn Jameson <59493115+CaitlynEJ@users.noreply.github.com>
2021-02-18 13:22:38 -0600

    updates to code

--- contentGenerator.py
+++ contentGenerator.py
@@ -1,63 +1,111 @@
 from tkinter import *
 import requests
 from bs4 import BeautifulSoup
+import random
+import csv
 
-root = Tk()
-root.title("Content Generator")
 
 # define generateParagrah function
-
-
-def generateParagrah():
+def generateParagraph():
     primaryKey = inputBox1.get()
     secondKey = inputBox2.get()
+    status = -1
+    global output
 
+    # find a wikipedia page about a topic
     response = requests.get(
-        url="https://en.wikipedia.org/wiki/" + primaryKey)
-
+        url="http://en.wikipedia.org/wiki/" + primaryKey,
+    )
     soup = BeautifulSoup(response.content, 'html.parser')
-    soup = soup.findall("p")
+
+    # collect all paragraphs
+    allParagraphs = soup.find_all('p')
+    #generate random paragraph order
+    random.shuffle(allParagraphs)
+    for paragraph in allParagraphs:
+
+        textPar = paragraph.text
+        # determine if primary key and second key are in the paragraph
+        if ((textPar.find(primaryKey) != -1) and (textPar.find(secondKey) != -1)):
+            output = textPar
+            status = 1
+            break
+        else:
+            continue
+
+    if status != 1:
+        output = "We could not find context that contains those two words. Please try again."
+        outputBox.insert(0, output)
+
+    if status != -1:
+        fields = ['input_words', 'output_content']
+        rows = [(primaryKey + "; " + secondKey), output]
+        outputBox.insert(0, output)
+        filename = "output.csv"
+        with open(filename, 'w') as csvfile:
+            csvWrite = csv.writer(csvfile)
+
+            csvWrite.writerow(fields)
+
+            csvWrite.writerow(rows)
 
 
-# define header
-header = Label(root, text="Content Generator")
+def main():
 
-inputFrame = LabelFrame(root, padx=10, pady=10)
-outputFrame = LabelFrame(root, padx=10, pady=10)
+    # define GUI window
+    root = Tk()
+    root.title("Content Generator")
 
-# define Input text box for primary keyword
-primaryKeyword = Label(inputFrame, text="Please enter a primary keyword.")
-inputBox1 = Entry(inputFrame, width=20, borderwidth=5)
-
-# define input text box for secondary keyword
-secondaryKeyword = Label(inputFrame, text="Please enter a secondary keyword.")
-inputBox2 = Entry(inputFrame, width=20, borderwidth=5)
-
-# define submit button
-submitButton = Button(inputFrame, text="Submit Keywords!",
-                      command=generateParagraph)
+    global inputBox1
+    global inputBox2
+    global outputBox
 
 
-# define output text box
-outputBox = Entry(outputFrame, borderwidth=5)
-resultHeader = Label(outputFrame, text="Generated results:")
+    # define header
+    header = Label(root, text="Content Generator")
 
-# layout grid
-inputFrame.grid(row=0, column=0, padx=10, pady=10)
-outputFrame.grid(row=1, column=0, padx=10, pady=10)
+    inputFrame = LabelFrame(root, padx=10, pady=10)
+    outputFrame = LabelFrame(root, padx=10, pady=10, )
 
-header.grid(row=0, column=0)
-primaryKeyword.grid(row=1, column=0)
-inputBox1.grid(row=1, column=1)
+    # define Input text box for primary keyword
+    primaryKeyword = Label(inputFrame, text="Please enter a primary keyword.")
+    inputBox1 = Entry(inputFrame, width=20, borderwidth=5)
 
-secondaryKeyword.grid(row=2, column=0)
-inputBox2.grid(row=2, column=1)
+    # define input text box for secondary keyword
+    secondaryKeyword = Label(inputFrame, text="Please enter a secondary keyword.")
+    inputBox2 = Entry(inputFrame, width=20, borderwidth=5)
 
-submitButton.grid(row=3, column=1)
-
-resultHeader.grid(row=4, column=0)
-outputBox.grid(row=5, column=0, columnspan=3,
-               padx=10, pady=10, ipadx=100, ipady=50)
+    # define submit button
+    submitButton = Button(inputFrame, text="Submit Keywords!",
+                          command=generateParagraph)
 
 
-root.mainloop()
+    # define output text box
+    outputBox = Entry(outputFrame, borderwidth=5, )
+    s = Scrollbar(outputFrame)
+
+    resultHeader = Label(outputFrame, text="Generated results:")
+
+    # layout grid
+    inputFrame.grid(row=0, column=0, padx=10, pady=10)
+    outputFrame.grid(row=1, column=0, padx=10, pady=10)
+
+    header.grid(row=0, column=0)
+    primaryKeyword.grid(row=1, column=0)
+    inputBox1.grid(row=1, column=1)
+
+    secondaryKeyword.grid(row=2, column=0)
+    inputBox2.grid(row=2, column=1)
+
+    submitButton.grid(row=3, column=1)
+
+    resultHeader.grid(row=4, column=0)
+    outputBox.grid(row=5, column=0, columnspan=3,
+                   padx=10, pady=10, ipadx=100, ipady=50)
+
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
